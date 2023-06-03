3b5819af9a4bd9e7b6e605e9d850b0894b6211c1
Nathan Griffiths <ngriffiths@postmedia.com>
2023-06-03 13:18:11 -0700

    geocode restricted to BC, popup shows combined air pollutants for clicked point

--- src/data/popup-template.js
+++ src/data/popup-template.js
@@ -1,18 +1,34 @@
-module.exports = (data) => {
-    // console.log(data.contaminants)
+module.exports = (data_obj) => {
     let list = '';
+    const data = data_obj.sumByContaminant;
 
-    data.contaminants[0].forEach(d => {
-        if (d.total === 0) return;
+    // we're iterating over an object (i know, i know <rolling_eyes>)
+    for (const d in data) {
         list += `
-                <li><p><span class="bold">${d.contaminant}:</span> ${d.total}</p></li>
+                <li>
+                    <ul>
+                        <span class="bold">${d}:</span>
+                        <p>Metric tonnes released:</span> ${data[d].total_released.toFixed(1)}</p>
+                        <p>Total health cost:</span> $${data[d].total_impact_value.toFixed(0)}</p>
+                        <p>Impact scale: ${data[d].total_impact_scale.toFixed(1)}</p>
+                    </ul>
+                </li>
             `;
-    });
+    }
+    // data.forEach(d => {
+    //     // if (d.total === 0) return;
+    //     list += `
+    //             <li>
+    //                 <p><span class="bold">${d.contaminant}:</span> $${Math.round(d.total_impact_value, 0)}</p>
+    //                 <p>Impact scale: ${Math.round(d.total_impact_scale * 10) / 10}</p>
+    //             </li>
+    //         `;
+    // });
 
     return `
         <div class="popup-container">
             <h2>${data.org}</h2>
-            <p class="subhead">Metric tonnes emitted, 2009-2021</p>
+            <p class="subhead">Totals values, 2009-2021</p>
             <ul class="popup-list">${list}</ul>
         </div>
     `;
--- src/js/components/Map/Map.js
+++ src/js/components/Map/Map.js
@@ -1,5 +1,8 @@
-import maplibregl from 'maplibre-gl';
+import Maplibregl from 'maplibre-gl';
+import * as turf from '@turf/helpers';
+import PointsWithinPolygon from '@turf/points-within-polygon';
 import MaplibreGeocoder from '@maplibre/maplibre-gl-geocoder';
+
 
 //TEMPLATES
 import popupTemplate from '../../../data/popup-template';
@@ -11,14 +14,14 @@ import '../../../css/popup.css';
 import '@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css';
 
 // VARS
-let map, popup;
+let buffers, map, popup;
 
 // FUNCTIONS
-async function init(options, facilities, buffers) {
-	console.log(facilities);
+async function init(options, facilities, buffers_1k) {
+	buffers = buffers_1k;
 
 	// setup the map
-	map = new maplibregl.Map({
+	map = new Maplibregl.Map({
 		antialias: true,
 		container: 'map',
 		style: options.mapboxStyle,
@@ -29,10 +32,10 @@ async function init(options, facilities, buffers) {
 	});
 
 	// setup address search
-	const geocoder = await setupGeocoder(map);
+	const geocoder = await setupGeocoder(map, options);
 
-	// setup popup for facilities
-	const popup = setupPopup(map);
+	// setup popup for buffer zoness
+	const popup = setupPopup(map, buffers);
 
 	// Add zoom, geocode, etc, to the map
 	addMapFeatures(map, geocoder);
@@ -68,7 +71,7 @@ async function init(options, facilities, buffers) {
 				paint: {
 					'circle-color': '#0062a3',
 					'circle-opacity': 0.8,
-					'circle-radius': 5,
+					'circle-radius': 3.5,
 					'circle-stroke-color': '#FFF',
 					'circle-stroke-width': 0.5
 				}
@@ -76,69 +79,43 @@ async function init(options, facilities, buffers) {
 	});
 }
 
-async function setupGeocoder(map) {
-		// create geolocator
-		const geocoder_api = {
-			forwardGeocode: async (config) => {
-				const features = [];
-				
-				try {
-					let request =
-					'https://nominatim.openstreetmap.org/search?q=' + config.query + '&format=geojson&polygon_geojson=1&addressdetails=1';
-					const response = await fetch(request);
-					const geojson = await response.json();
-					
-					for (let feature of geojson.features) {
-						let center = [
-							feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
-							feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2
-						];
-	
-						let point = {  
-								type: 'Feature',
-								geometry: {
-								type: 'Point',
-								coordinates: center
-							},
-							place_name: feature.properties.display_name,
-							properties: feature.properties,
-							text: feature.properties.display_name,
-							place_type: ['place'],
-							center: center
-						};
-					
-						features.push(point);
-					}
-				} catch (e) {
-					console.error(`Failed to forwardGeocode with error: ${e}`);
-				}
-				
-				return {
-					features: features
+function addBufferValues(data) {
+	console.log(data)
+	const data_obj = {
+		orgs: []
+	};
+	const sumByContaminant = {};
+	// console.log(JSON.stringify(data))
+
+	data.forEach(d => {
+		data_obj.orgs.push(d.properties.organization);
+		d.properties.data[0].forEach(obj => {
+			const { contaminant, total_released, total_impact_value, total_impact_scale } = obj;
+			if (sumByContaminant.hasOwnProperty(contaminant)) {
+				sumByContaminant[contaminant].total_released += total_released;
+				sumByContaminant[contaminant].total_impact_scale += total_impact_scale;
+				sumByContaminant[contaminant].total_impact_value += total_impact_value;
+			} else {
+				sumByContaminant[contaminant] = {
+					total_released,
+					total_impact_scale,
+					total_impact_value
 				};
 			}
-		};
-	
-		const geocoder = new MaplibreGeocoder(geocoder_api, {
-			maplibregl: maplibregl,
-			placeholder: 'Enter an address...'
 		});
-		// default zoom is too close
-		geocoder.on('result', e => {
-			map.flyTo({
-				center: e.result.center,
-				zoom: options.geocodeZoomLevel
-			});
-		});
+	});
 
-	return geocoder;
+	data_obj.sumByContaminant = sumByContaminant;
+
+	// console.log(sumByContaminant);
+	return data_obj;
 }
 
 function addMapFeatures(map, geocoder) {
 	map
 		// geolocate control
 		.addControl(
-			new maplibregl.GeolocateControl({
+			new Maplibregl.GeolocateControl({
 				positionOptions: {
 					enableHighAccuracy: true
 				},
@@ -148,21 +125,120 @@ function addMapFeatures(map, geocoder) {
 		.addControl(geocoder)
 		// zoom
 		.addControl(
-			new maplibregl.NavigationControl()
+			new Maplibregl.NavigationControl()
 		);
 
 }
 
+async function setupGeocoder(map, options) {
+	// create geolocator
+	const geocoder_api = {
+		forwardGeocode: async (config) => {
+			const features = [];
+			
+			try {
+				let request =
+				'https://nominatim.openstreetmap.org/search?q=' + config.query + '&format=geojson&polygon_geojson=1&addressdetails=1';
+				const response = await fetch(request);
+				const geojson = await response.json();
+				
+				for (let feature of geojson.features) {
+					let center = [
+						feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
+						feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2
+					];
+
+					let point = {  
+							type: 'Feature',
+							geometry: {
+							type: 'Point',
+							coordinates: center
+						},
+						place_name: feature.properties.display_name,
+						properties: feature.properties,
+						text: feature.properties.display_name,
+						place_type: ['place'],
+						center: center
+					};
+				
+					features.push(point);
+				}
+			} catch (e) {
+				console.error(`Failed to forwardGeocode with error: ${e}`);
+			}
+			
+			return {
+				features: features
+			};
+		}
+	};
+
+	const geocoder = new MaplibreGeocoder(geocoder_api, {
+		clearOnBlur: true,
+		// Filter results to only include addresses in Canada
+		countries: 'ca',
+		filter: item => { return item.properties.address['ISO3166-2-lvl4'] === 'CA-BC' },
+		maplibregl: Maplibregl,
+		placeholder: 'Lookup an address...'
+	});
+	// default zoom is too close
+	geocoder.on('result', e => {
+		map.flyTo({
+		// map.easeTo({
+			center: e.result.center,
+			// this animation is considered essential with respect to prefers-reduced-motion
+			essential: true,
+			zoom: options.geocodeZoomLevel
+		})
+	}, () => {
+		// this doesn't trigger... hmm....
+		console.log('Done!')
+	});
+
+return geocoder;
+}
+
 function setupPopup(map) {
 	// create a popup but don't add it to the map yet...
-	popup = new maplibregl.Popup({
+	popup = new Maplibregl.Popup({
 		closeButton: true,
 		closeonClick: false
 	});
 
 	// mouseevents for popup
-	map.on('mouseenter', 'buffers-1k', showPopup);
-	map.on('click', 'buffers-1k', showPopup);
+	// map.on('mouseenter', 'buffers-1k', showPopup);
+	// map.on('click', 'buffers-1k', showPopup);
+	map.on('click', showPopup_v2)
+}
+
+function showPopup_v2(e) {
+	const data = [];
+	// console.log(e.lngLat);
+	// create a geojson of our point
+	const point = turf.point([e.lngLat.lng, e.lngLat.lat]);
+
+	// find out if the point is inside buffers
+	buffers.buffers_1k.features.forEach(d => {
+		// is point within a buffer polygon?
+		const withinPoly = PointsWithinPolygon(point, d);
+		// if so, let's cache the buffer
+		if (withinPoly.features.length > 0) data.push(d);
+	});
+
+	// add up all the values from the overlapping buffers
+	const totals = addBufferValues(data);
+
+	// we don't need a popup if we're not inside a buffer...
+	if (totals.orgs.length === 0) return;
+
+	// fill in the popup template
+	const html = popupTemplate(totals);
+
+	// populate the popup, set coordinates & display on map
+	popup
+		.setLngLat(e.lngLat)
+		.setHTML(html)
+		.addTo(map);
 }
 
 function showPopup(e) {
