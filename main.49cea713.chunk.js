f4ab72e0183f6dea99d17fca75f87fcc7cd09b12
Nathan Griffiths <ngriffiths@postmedia.com>
2023-07-10 16:31:02 -0700

    Updates

--- main.49cea713.chunk.js
+++ main.49cea713.chunk.js
@@ -110,15 +110,14 @@ var lib_default = /*#__PURE__*/__webpack_require__.n(lib);
 // CONCATENATED MODULE: ./src/data/popup-template.js
 function popupTemplate(data_obj) {
   let list = '';
-  const data = data_obj.sumByContaminant; // we're iterating over an object (i know, i know <rolling_eyes>)
+  const data = data_obj.sumByContaminant;
+  console.log(data_obj); // we're iterating over an object (i know, i know <rolling_eyes>)
 
   for (const d in data) {
-    console.log(data[d].total_released);
+    console.log(d);
     const released = data[d].total_released.toFixed(2).toLocaleString('en-US');
     const cost = data[d].total_impact_value.toLocaleString('en-US').split('.')[0];
     const scale = data[d].total_impact_scale.toFixed(1).toLocaleString();
-    console.log(data[d].total_impact_value);
-    console.log(released);
     list += `
                 <li>
                     <ul>
@@ -133,7 +132,7 @@ function popupTemplate(data_obj) {
 
   return `
         <div class="popup-container">
-            <h2>${data.org}</h2>
+            <h2>${data_obj.orgs[0]}</h2>
             <p class="subhead">Total values, 2009-2021</p>
             <ul class="popup-list">${list}</ul>
         </div>
